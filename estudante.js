ae60631f3982fdb9e4a562b6f27d763d892aad5a
Matheus Antunes Monteiro <math.am@hotmail.com>
2022-05-18 11:16:18 -0300

    Imprimindo média do estudante

--- estudante.js
+++ estudante.js
@@ -7,3 +7,13 @@ let nota2 = 8.5;
 let nota3 = 6.9;
 
 let mediaAluno = (nota1 * 4 + nota2 * 3 + nota3 * 3) / (4 + 3 + 3);
+
+console.log(nomeAluno, idadeAluno, mediaAluno);
+
+if (mediaAluno >= 7) {
+  console.log("Aluno aprovado");
+} else if (mediaAluno !== 0) {
+  console.log("Aluno reprovado");
+} else {
+  console.log("Aluno zerou");
+}
